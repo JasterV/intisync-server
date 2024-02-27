b2165c42522c611b97da16ea8e62ccc94b6d415c
Víctor Martínez <49537445+JasterV@users.noreply.github.com>
2024-02-28 00:14:13 +0100

    refactor: move actors tests & use test-context (#7)

--- src/actors/controller.rs
+++ src/actors/controller.rs
@@ -46,3 +46,321 @@ where
         on_disconnect(ClientSocketImpl::from(socket), sessions.0).await
     });
 }
+
+#[cfg(test)]
+mod tests {
+    use super::on_join_session;
+    use crate::{
+        actors::controller::{
+            JoinSessionErrorKind, JoinSessionPermissionRequest, JoinSessionPermissionResponse,
+            JoinSessionRequest, JoinSessionResponse,
+        },
+        configuration::Config,
+        sessions::port::{MockSessionStore, SessionState},
+        socket::port::{DummyMockError, MockClientSocket, MockGlobalSocket},
+    };
+    use futures_util::FutureExt;
+    use mockall::predicate::eq;
+    use socketioxide::extract::Data;
+    use std::time::Duration;
+    use test_context::{test_context, AsyncTestContext};
+    use uuid::Uuid;
+
+    struct Context {
+        client_socket: MockClientSocket,
+        session_store: MockSessionStore,
+        global_socket: MockGlobalSocket,
+    }
+
+    impl AsyncTestContext for Context {
+        async fn setup() -> Self {
+            Self {
+                client_socket: MockClientSocket::new(),
+                global_socket: MockGlobalSocket::new(),
+                session_store: MockSessionStore::new(),
+            }
+        }
+    }
+
+    #[test_context(Context, skip_teardown)]
+    #[tokio::test]
+    async fn can_not_join_a_session_if_already_in_a_session(mut ctx: Context) {
+        let join_request = Data(JoinSessionRequest {
+            session_id: Uuid::nil(),
+            message: "hello world".into(),
+        });
+        let config = Config::load();
+
+        ctx.client_socket
+            .expect_get_stored_value()
+            .times(1)
+            .return_const(Some(Uuid::nil()));
+
+        let result = on_join_session(
+            ctx.client_socket,
+            ctx.global_socket,
+            join_request,
+            &ctx.session_store,
+            &config,
+        )
+        .await;
+
+        assert_eq!(
+            result,
+            JoinSessionResponse::with_err(JoinSessionErrorKind::AlreadyInASession)
+        );
+    }
+
+    #[test_context(Context, skip_teardown)]
+    #[tokio::test]
+    async fn can_not_join_non_existent_session(mut ctx: Context) {
+        let join_request = Data(JoinSessionRequest {
+            session_id: Uuid::nil(),
+            message: "hello world".into(),
+        });
+        let config = Config::load();
+
+        ctx.client_socket
+            .expect_get_stored_value()
+            .times(1)
+            .return_const(None);
+
+        ctx.session_store
+            .expect_session_state()
+            .times(1)
+            .returning(|_| async { Ok(None) }.boxed());
+
+        let result = on_join_session(
+            ctx.client_socket,
+            ctx.global_socket,
+            join_request,
+            &ctx.session_store,
+            &config,
+        )
+        .await;
+
+        assert_eq!(
+            result,
+            JoinSessionResponse::with_err(JoinSessionErrorKind::SessionNotFound)
+        );
+    }
+
+    #[test_context(Context, skip_teardown)]
+    #[tokio::test]
+    async fn can_not_join_a_full_session(mut ctx: Context) {
+        let join_request = Data(JoinSessionRequest {
+            session_id: Uuid::nil(),
+            message: "hello world".into(),
+        });
+        let config = Config::load();
+
+        ctx.client_socket
+            .expect_get_stored_value()
+            .times(1)
+            .return_const(None);
+
+        ctx.session_store
+            .expect_session_state()
+            .times(1)
+            .returning(|_| async { Ok(Some(SessionState::InProgress)) }.boxed());
+
+        let result = on_join_session(
+            ctx.client_socket,
+            ctx.global_socket,
+            join_request,
+            &ctx.session_store,
+            &config,
+        )
+        .await;
+
+        assert_eq!(
+            result,
+            JoinSessionResponse::with_err(JoinSessionErrorKind::SessionFull)
+        );
+    }
+
+    #[test_context(Context, skip_teardown)]
+    #[tokio::test]
+    async fn can_join_a_session_if_hub_accepts(mut ctx: Context) {
+        let join_request = JoinSessionRequest {
+            session_id: Uuid::nil(),
+            message: "hello world".into(),
+        };
+        let config = Config::load();
+
+        ctx.client_socket
+            .expect_get_stored_value()
+            .times(1)
+            .return_const(None);
+
+        ctx.session_store
+            .expect_session_state()
+            .times(1)
+            .returning(|_| async { Ok(Some(SessionState::WaitingForController)) }.boxed());
+
+        ctx.global_socket
+            .expect_emit_to_room_with_ack()
+            .times(1)
+            .with(
+                eq(join_request.session_id.to_string()),
+                eq("join_request".to_string()),
+                eq(JoinSessionPermissionRequest {
+                    message: join_request.message.clone(),
+                }),
+                eq(Duration::from_secs(
+                    config.controller.session_join_request_timeout,
+                )),
+            )
+            .returning(|_, _, _, _| async { Ok(JoinSessionPermissionResponse::Accept) }.boxed());
+
+        ctx.client_socket
+            .expect_join()
+            .times(1)
+            .return_const(Ok(()));
+
+        ctx.session_store
+            .expect_update_session_state()
+            .times(1)
+            .with(eq(join_request.session_id), eq(SessionState::InProgress))
+            .returning(|_, _| async { Ok(()) }.boxed());
+
+        ctx.client_socket
+            .expect_emit_to_room()
+            .times(1)
+            .with(
+                eq(join_request.session_id.to_string()),
+                eq("controller_joined".to_string()),
+                eq(()),
+            )
+            .return_const(Ok(()));
+
+        ctx.client_socket
+            .expect_store_value()
+            .times(1)
+            .with(eq(join_request.session_id))
+            .return_const(());
+
+        let result = on_join_session(
+            ctx.client_socket,
+            ctx.global_socket,
+            Data(join_request),
+            &ctx.session_store,
+            &config,
+        )
+        .await;
+
+        assert_eq!(result, JoinSessionResponse::Ok {});
+    }
+
+    #[test_context(Context, skip_teardown)]
+    #[tokio::test]
+    async fn do_not_join_a_session_if_hub_rejects(mut ctx: Context) {
+        let join_request = JoinSessionRequest {
+            session_id: Uuid::nil(),
+            message: "hello world".into(),
+        };
+        let config = Config::load();
+
+        ctx.client_socket
+            .expect_get_stored_value()
+            .times(1)
+            .return_const(None);
+
+        ctx.session_store
+            .expect_session_state()
+            .times(1)
+            .returning(|_| async { Ok(Some(SessionState::WaitingForController)) }.boxed());
+
+        ctx.global_socket
+            .expect_emit_to_room_with_ack()
+            .times(1)
+            .with(
+                eq(join_request.session_id.to_string()),
+                eq("join_request".to_string()),
+                eq(JoinSessionPermissionRequest {
+                    message: join_request.message.clone(),
+                }),
+                eq(Duration::from_secs(
+                    config.controller.session_join_request_timeout,
+                )),
+            )
+            .returning(|_, _, _, _| async { Ok(JoinSessionPermissionResponse::Reject) }.boxed());
+
+        ctx.client_socket.expect_join().never();
+        ctx.session_store.expect_update_session_state().never();
+        ctx.client_socket.expect_emit_to_room::<()>().never();
+        ctx.client_socket.expect_store_value().never();
+
+        let result = on_join_session(
+            ctx.client_socket,
+            ctx.global_socket,
+            Data(join_request),
+            &ctx.session_store,
+            &config,
+        )
+        .await;
+
+        assert_eq!(
+            result,
+            JoinSessionResponse::Error {
+                kind: JoinSessionErrorKind::Rejected
+            }
+        );
+    }
+
+    #[test_context(Context, skip_teardown)]
+    #[tokio::test]
+    async fn return_hub_timeout_error_if_hub_returns_an_error(mut ctx: Context) {
+        let join_request = JoinSessionRequest {
+            session_id: Uuid::nil(),
+            message: "hello world".into(),
+        };
+        let config = Config::load();
+
+        ctx.client_socket
+            .expect_get_stored_value()
+            .times(1)
+            .return_const(None);
+
+        ctx.session_store
+            .expect_session_state()
+            .times(1)
+            .returning(|_| async { Ok(Some(SessionState::WaitingForController)) }.boxed());
+
+        ctx.global_socket
+            .expect_emit_to_room_with_ack()
+            .times(1)
+            .with(
+                eq(join_request.session_id.to_string()),
+                eq("join_request".to_string()),
+                eq(JoinSessionPermissionRequest {
+                    message: join_request.message.clone(),
+                }),
+                eq(Duration::from_secs(
+                    config.controller.session_join_request_timeout,
+                )),
+            )
+            .returning(|_, _, _, _| async { Err(DummyMockError) }.boxed());
+
+        ctx.client_socket.expect_join().never();
+        ctx.session_store.expect_update_session_state().never();
+        ctx.client_socket.expect_emit_to_room::<()>().never();
+        ctx.client_socket.expect_store_value().never();
+
+        let result = on_join_session(
+            ctx.client_socket,
+            ctx.global_socket,
+            Data(join_request),
+            &ctx.session_store,
+            &config,
+        )
+        .await;
+
+        assert_eq!(
+            result,
+            JoinSessionResponse::Error {
+                kind: JoinSessionErrorKind::HubResponseTimeout
+            }
+        );
+    }
+}
--- src/actors/hub.rs
+++ src/actors/hub.rs
@@ -24,3 +24,127 @@ where
         on_disconnect(ClientSocketImpl::from(socket), sessions.0).await
     });
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{on_disconnect, on_start_session};
+    use crate::{
+        actors::hub::messages::{StartSessionError, StartSessionResponse},
+        sessions::port::MockSessionStore,
+        socket::port::MockClientSocket,
+    };
+    use mockall::predicate::eq;
+    use test_context::{test_context, AsyncTestContext};
+    use uuid::Uuid;
+
+    struct Context {
+        client_socket: MockClientSocket,
+        session_store: MockSessionStore,
+    }
+
+    impl AsyncTestContext for Context {
+        async fn setup() -> Self {
+            Self {
+                client_socket: MockClientSocket::new(),
+                session_store: MockSessionStore::new(),
+            }
+        }
+    }
+
+    #[test_context(Context, skip_teardown)]
+    #[tokio::test]
+    async fn creates_a_session_on_start_session_command(mut ctx: Context) {
+        ctx.client_socket
+            .expect_get_stored_value()
+            .times(1)
+            .return_const(None);
+
+        ctx.session_store
+            .expect_create_session()
+            .times(1)
+            .returning(|| Box::pin(async move { Ok(Uuid::nil()) }));
+
+        ctx.client_socket
+            .expect_join()
+            .with(eq(Uuid::nil().to_string()))
+            .times(1)
+            .return_const(Ok(()));
+
+        ctx.client_socket
+            .expect_store_value()
+            .with(eq(Uuid::nil()))
+            .times(1)
+            .return_const(());
+
+        let result = on_start_session(ctx.client_socket, &ctx.session_store).await;
+
+        assert!(matches!(result, StartSessionResponse::Ok { session_id: _ }));
+    }
+
+    #[test_context(Context, skip_teardown)]
+    #[tokio::test]
+    async fn does_not_create_a_session_if_already_in_a_session(mut ctx: Context) {
+        ctx.client_socket
+            .expect_get_stored_value()
+            .times(1)
+            .return_const(Some(Uuid::nil()));
+
+        ctx.session_store.expect_create_session().never();
+        ctx.client_socket.expect_join().never();
+        ctx.client_socket.expect_store_value().never();
+
+        let result = on_start_session(ctx.client_socket, &ctx.session_store).await;
+
+        assert_eq!(
+            result,
+            StartSessionResponse::error(StartSessionError::AlreadyInASession)
+        );
+    }
+
+    #[test_context(Context, skip_teardown)]
+    #[tokio::test]
+    async fn deletes_session_and_sends_message_on_disconnect_if_in_a_session(mut ctx: Context) {
+        ctx.client_socket
+            .expect_get_stored_value()
+            .times(1)
+            .return_const(Some(Uuid::nil()));
+
+        ctx.client_socket
+            .expect_emit_to_room()
+            .times(1)
+            .with(
+                eq(Uuid::nil().to_string()),
+                eq("session_finished".to_string()),
+                eq(()),
+            )
+            .return_const(Ok(()));
+
+        ctx.client_socket
+            .expect_remove_value()
+            .times(1)
+            .return_const(());
+
+        ctx.session_store
+            .expect_delete_session()
+            .times(1)
+            .with(eq(Uuid::nil()))
+            .returning(|_| Box::pin(async { Ok(()) }));
+
+        on_disconnect(ctx.client_socket, &ctx.session_store).await;
+    }
+
+    #[test_context(Context, skip_teardown)]
+    #[tokio::test]
+    async fn doesnt_do_anything_if_not_in_a_session_when_disconnect(mut ctx: Context) {
+        ctx.client_socket
+            .expect_get_stored_value()
+            .times(1)
+            .return_const(None);
+
+        ctx.client_socket.expect_emit_to_room::<()>().never();
+        ctx.client_socket.expect_remove_value().never();
+        ctx.session_store.expect_delete_session().never();
+
+        on_disconnect(ctx.client_socket, &ctx.session_store).await;
+    }
+}
